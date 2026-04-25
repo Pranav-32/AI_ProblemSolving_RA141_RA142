1b66272fb51fef016d0213bd551caf687849867b
IceBear-32 <icebearfromsouthpole@gmail.com>
2026-04-25 16:15:59 +0530

    Tic-Tac-Toe game, with minmax and alpha-beta pruning

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from problem1_tictactoe.logic import best_move_minimax, best_move_alpha_beta, check_winner
 
 app = Flask(
     __name__,
@@ -9,6 +10,37 @@ app = Flask(
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/tic-tac-toe')
+def tic_tac_toe():
+    return render_template('tictactoe.html')
+
+@app.post('/api/tictactoe/move')
+def tictactoe_move():
+    data = request.get_json(force=True)
+    board = data.get('board', [''] * 9)
+    method = data.get('method', 'alphabeta')
+
+    if check_winner(board) or '' not in board:
+        return jsonify({'board': board, 'winner': check_winner(board), 'move': None})
+
+    if method == 'minimax':
+        result = best_move_minimax(board, ai='O', human='X')
+    else:
+        result = best_move_alpha_beta(board, ai='O', human='X')
+
+    move = result['move']
+    if move is not None:
+        board[move] = 'O'
+
+    return jsonify({
+        'board': board,
+        'move': move,
+        'winner': check_winner(board),
+        'nodes': result['nodes'],
+        'time_ms': round(result['time_ms'], 4),
+        'method': method
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
